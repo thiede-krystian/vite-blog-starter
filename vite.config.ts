cabdecfcd8fe94dd01c887736177a5bf9bd8e679
thiede-krystian <krystian.thiede@protonmail.com>
2024-06-12 11:07:28 +0200

    chore: add changes to vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  build: {
+    sourcemap: true,
+  },
 });
